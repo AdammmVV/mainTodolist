2cb794574a6b53c886998a535cdaec723574191d
Adammm <adammm.web@gmail.com>
2023-01-29 00:56:43 +0300

    change SuperInput.tsx (logic)

--- src/components/SuperInput/SuperInput.tsx
+++ src/components/SuperInput/SuperInput.tsx
@@ -1,11 +1,21 @@
-import React, {ChangeEvent} from "react";
+import React, {ChangeEvent, KeyboardEvent} from "react";
 
 type SuperInputPropsType = {
+    onKeyDown: (e: KeyboardEvent<HTMLInputElement>) => void
     onChange: (e: string) => void
     title: string
 
 }
-export const SuperInput: React.FC<SuperInputPropsType> = ({onChange, title}) => {
+export const SuperInput: React.FC<SuperInputPropsType> = (
+    {
+        onKeyDown,
+        onChange,
+        title
+    }) => {
+
+    const onKeyDownHandler = (e: KeyboardEvent<HTMLInputElement>) => {
+        onKeyDown(e)
+    }
 
     const onChangeHandler = (e: ChangeEvent<HTMLInputElement>) => {
         onChange(e.currentTarget.value)
@@ -13,6 +23,7 @@ export const SuperInput: React.FC<SuperInputPropsType> = ({onChange, title}) => 
 
     return (
         <input
+            onKeyDown={onKeyDownHandler}
             onChange={onChangeHandler}
             value={title}
             type="text"/>
