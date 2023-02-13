670a94a1ecea974b9d5184585bde9d78c08e7894
Adammm <adammm.web@gmail.com>
2023-02-14 00:25:17 +0300

    add logic for change Task in SuperSpan.tsx

--- src/components/SuperSpan.tsx
+++ src/components/SuperSpan.tsx
@@ -1,18 +1,37 @@
-import React, {useState} from "react";
+import React, {ChangeEvent, useState} from "react";
 
 type SuperSpanPropsType = {
     title: string
+    callBack: (newTitle: string) => void
 }
 export const SuperSpan: React.FC<SuperSpanPropsType> =
     ({
          title,
+        callBack,
      }) => {
         const [spanValue, setSpanValue] = useState<string>(title)
+        const [inputState, setInputState] = useState(false)
+
+        const onDoubleClickSpanHandler = () => {
+            setInputState(true)
+        }
+
+        const onBlurInputHandler = () => {
+            setInputState(false)
+            callBack(spanValue)
+        }
+
+        const onChangeInputHandler = (e:ChangeEvent<HTMLInputElement>) => {
+            setSpanValue(e.currentTarget.value)
+        }
 
         return (
-            <>
-                <input type="text"/>
-                <span>{spanValue}</span>
-            </>
+            inputState
+                ? <input value={spanValue}
+                         onChange={onChangeInputHandler}
+                         onBlur={onBlurInputHandler}
+                         autoFocus
+                         type="text"/>
+                : <span onDoubleClick={onDoubleClickSpanHandler}>{spanValue}</span>
         )
     }
