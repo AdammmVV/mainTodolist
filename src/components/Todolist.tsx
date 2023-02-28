a1c392df02eda5313498046a227b7a20e15d43cd
Adammm <adammm.web@gmail.com>
2023-02-28 12:25:18 +0300

    add MUI for Todolist.tsx

--- src/components/Todolist.tsx
+++ src/components/Todolist.tsx
@@ -1,11 +1,14 @@
 import {SuperButton} from "./SuperButton/SuperButton";
-import React from "react";
+import s from './Todolist.module.css'
+import React, {ChangeEvent} from "react";
 import {FilterType} from "../App";
 import {SuperInputCheckBox} from "./SuperInputCheckBox/SuperInputCheckBox";
 import {SuperSpan} from "./SuperSpan";
 import {AddItemForm} from "./AddItemForm";
 import {TasksType} from "../redux/tasksReducer";
-
+import {Checkbox, IconButton} from '@mui/material';
+import DeleteForeverSharpIcon from '@mui/icons-material/DeleteForeverSharp';
+import HighlightOffSharpIcon from '@mui/icons-material/HighlightOffSharp';
 
 type TodolistPropsType = {
     titleTodo: string
@@ -53,41 +56,50 @@ export const Todolist: React.FC<TodolistPropsType> = (
     }
 
     return (
-        <div>
+        <div className={s.todoListWrapper}>
             <h3>
                 <SuperSpan title={titleTodo} callBack={getNewTitleTodoList}/>
-                <SuperButton onClick={removeTodoListHandler} name={'X'}/>
+                <IconButton onClick={removeTodoListHandler} color={'primary'} size={'medium'}>
+                    <HighlightOffSharpIcon/>
+                </IconButton>
             </h3>
             <div>
-                <AddItemForm getTitle={getNetNewTaskTitle}/>
+                <AddItemForm getTitle={getNetNewTaskTitle} label={'Add task'}/>
             </div>
             <div>
-                <ul>
-                    {tasks.map(t => {
-                            const onChangeInputCheckBox = (isDone: boolean) => {
-                                setCheckbox(todoListId, t.id, isDone)
+                <ul className={s.tasksWrapper}>
+                    {tasks.map((t, i) => {
+                            const onChangeInputCheckBox = (e: ChangeEvent<HTMLInputElement>) => {
+                                setCheckbox(todoListId, t.id, e.currentTarget.checked)
                             }
                             const removeTaskHandler = () => removeTask(todoListId, t.id)
                             const getNewTitle = (newTitle: string) => getNewTitleTask(t.id, newTitle)
                             return (
-                                <li key={t.id} className={t.isDone ? 'done' : ''}>
-                                    <SuperInputCheckBox onChange={onChangeInputCheckBox} checked={t.isDone}/>
-                                    <SuperSpan title={t.title} callBack={getNewTitle}/>
-                                    <SuperButton onClick={removeTaskHandler} name={'X'}/>
+                                <li key={t.id}
+                                    className={`${t.isDone ? `${s.taskWrapper} ${s.done}` : s.taskWrapper} ${i%2 === 0 ? s.backgroundTask : ''}`}>
+                                    <div>
+                                        <Checkbox onChange={onChangeInputCheckBox} checked={t.isDone}/>
+                                        <SuperSpan title={t.title} callBack={getNewTitle}/>
+                                    </div>
+                                    <IconButton onClick={removeTaskHandler} color={"primary"}>
+                                        <DeleteForeverSharpIcon/>
+                                    </IconButton>
                                 </li>)
                         }
                     )}
-
                 </ul>
             </div>
-            <div>
-                <SuperButton background={filter === 'all'}
+            <div className={s.buttonWrapper}>
+                <SuperButton color={filter === 'all' ? 'secondary' : undefined}
+                             variant={'contained'}
                              onClick={() => tasksFilter(todoListId, 'all')}
                              name={'All'}/>
-                <SuperButton background={filter === 'active'}
+                <SuperButton color={filter === 'active' ? 'secondary' : undefined}
+                             variant={'contained'}
                              onClick={() => tasksFilter(todoListId, 'active')}
                              name={'Active'}/>
-                <SuperButton background={filter === 'completed'}
+                <SuperButton color={filter === 'completed' ? 'secondary' : undefined}
+                             variant={'contained'}
                              onClick={() => tasksFilter(todoListId, 'completed')}
                              name={'Completed'}/>
             </div>
