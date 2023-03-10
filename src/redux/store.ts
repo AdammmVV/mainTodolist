8190797c084416de6121c4f751a83e24a6d0c8c4
Adammm <adammm.web@gmail.com>
2023-03-11 00:04:16 +0300

    translation application to Redux create store.ts

---
+++ src/redux/store.ts
@@ -0,0 +1,15 @@
+import {combineReducers, legacy_createStore} from "redux";
+import {todoListReducer} from "./todoListReducer";
+import {tasksReducer} from "./tasksReducer";
+
+const rootReducer = combineReducers({
+    todoLists: todoListReducer,
+    tasks: tasksReducer
+})
+
+export const store = legacy_createStore(rootReducer)
+
+export type AppRootStateType = ReturnType<typeof rootReducer>
+
+//@ts-ignore
+window.store = store
