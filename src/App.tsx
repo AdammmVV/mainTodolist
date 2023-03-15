dd7efefc4b38f104c74c14d30a0cc47b91933dd2
Adammm <adammm.web@gmail.com>
2023-03-16 00:28:33 +0300

    refactor App.tsx

--- src/App.tsx
+++ src/App.tsx
@@ -1,28 +1,15 @@
-import React, {useState} from 'react';
+import React, {useCallback, useState} from 'react';
 import './App.css';
 import {v1} from "uuid";
 import {Todolist} from "./components/Todolist";
 import {AddItemForm} from "./components/AddItemForm";
 import {addNewTodoListAC, TodolistType} from "./redux/todoListReducer";
 import {addNewTodoListAndTaskAC} from "./redux/tasksReducer";
-import {
-    AppBar,
-    Button,
-    Container,
-    createTheme,
-    CssBaseline,
-    Grid,
-    IconButton,
-    Paper,
-    Switch,
-    ThemeProvider,
-    Toolbar,
-    Typography
-} from "@mui/material";
+import {Container, createTheme, CssBaseline, Grid, Paper, ThemeProvider} from "@mui/material";
 import {amber, deepOrange} from "@mui/material/colors";
-import MenuIcon from '@mui/icons-material/Menu';
 import {useDispatch, useSelector} from "react-redux";
 import {AppRootStateType} from "./redux/store";
+import {Header} from "./components/Header/Header";
 
 export type FilterType = 'all' | 'active' | 'completed'
 
@@ -43,11 +30,11 @@ export const App = () => {
     const todoLists = useSelector<AppRootStateType, TodolistType[]>(state => state.todoLists)
     const dispatch = useDispatch()
 
-    const addNewTodoList = (title: string) => {
+    const addNewTodoList = useCallback((title: string) => {
         let todoListId = v1()
         dispatch(addNewTodoListAC(todoListId, title))
         dispatch(addNewTodoListAndTaskAC(todoListId))
-    }
+    }, [dispatch])
 
     const mapTodolist = todoLists.map(todo => {
         return (
@@ -70,36 +57,13 @@ export const App = () => {
     })
     return (
         <ThemeProvider theme={dark ? darkTheme : waitTheme}>
-            <AppBar position="static" color={'primary'}>
-                <Toolbar variant={'dense'}>
-                    <IconButton
-                        size="large"
-                        edge="start"
-                        color="inherit"
-                        aria-label="menu"
-                        sx={{ mr: 2 }}
-                    >
-                        <MenuIcon />
-                    </IconButton>
-                    <Typography variant="h6" component="div" sx={{ flexGrow: 1 }}>
-                        News
-                    </Typography>
-                        <Switch onChange={(e) => {setDark(e.currentTarget.checked)}} color={'secondary'}/>
-                    <Button color="inherit">Login</Button>
-                </Toolbar>
-            </AppBar>
-            <CssBaseline/>
-            <Container fixed sx={{
-                p:10,
-            }}>
+            <Header setDark={setDark}/>
+            <Container fixed sx={{p: 10}}>
+                <CssBaseline/>
                 <Grid container spacing={5} direction={'column'} alignItems={'center'} justifyItems={'center'}>
+                    <AddItemForm getTitle={addNewTodoList} label={'Add Todo'}/>
                     <Grid item>
-                        <div>
-                            <AddItemForm getTitle={addNewTodoList} label={'Add Todo'}/>
-                        </div>
-                    </Grid>
-                    <Grid item>
-                        <Grid container >
+                        <Grid container justifyItems={'center'}>
                             {mapTodolist}
                         </Grid>
                     </Grid>
@@ -108,5 +72,4 @@ export const App = () => {
         </ThemeProvider>
     )
 }
-
 
