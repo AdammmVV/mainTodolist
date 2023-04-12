16e2c9735d7b34ec746f29d3a2d17ebdbcaf861b
Adammm <adammm.web@gmail.com>
2023-04-12 14:47:58 +0300

    refactor create MUIButton.tsx

---
+++ src/components/SuperButton/MUIButton.tsx
@@ -0,0 +1,17 @@
+import React from "react";
+import {Button} from "@mui/material";
+
+type SuperButtonPropsType = {
+    onClick: () => void
+    name: string
+    color?:  "inherit" | "primary" | "secondary" | "success" | "error" | "info" | "warning" | undefined
+    variant?: "text" | "outlined" | "contained" | undefined
+}
+export const MUIButton = React.memo(  (props: SuperButtonPropsType) => {
+    return (
+        <Button color={props.color}
+                variant={props.variant}
+                onClick={props.onClick}
+        >{props.name}</Button>
+    )
+})
