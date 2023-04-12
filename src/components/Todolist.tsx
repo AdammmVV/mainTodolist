865da1a760857ac1f9db7619089c7aa8e03c32c0
Adammm <adammm.web@gmail.com>
2023-04-12 14:50:08 +0300

    refactor Todolist.tsx

--- src/components/Todolist.tsx
+++ src/components/Todolist.tsx
@@ -1,16 +1,15 @@
-import {SuperButton} from "./SuperButton/SuperButton";
+import {MUIButton} from "./SuperButton/MUIButton";
 import s from './Todolist.module.css'
 import React, {memo, useCallback} from "react";
 import {FilterType} from "../App";
 import {SuperSpan} from "./SuperSpan";
 import {AddItemForm} from "./AddItemForm";
-import {addTaskAC, removeTasksAC, TasksStateType, TasksType} from "../redux/tasksReducer";
-import {IconButton} from '@mui/material';
-import HighlightOffSharpIcon from '@mui/icons-material/HighlightOffSharp';
+import {addTaskAC, removeTasksAC, TasksType} from "../redux/tasksReducer";
 import {useDispatch, useSelector} from "react-redux";
 import {AppRootStateType} from "../redux/store";
 import {changedTodoListTitleAC, removeTodoListAC, tasksFilterAC} from "../redux/todoListReducer";
 import {Task} from "./Task";
+import {IconMUIButton} from "./SuperButton/IconMUIButton";
 
 type TodolistPropsType = {
     titleTodo: string
@@ -25,33 +24,28 @@ export const Todolist: React.FC<TodolistPropsType> = memo((
         filter,
     }
 ) => {
-
-    const tasks = useSelector<AppRootStateType, TasksStateType>(state => state.tasks)
+    let tasks = useSelector<AppRootStateType, TasksType[]>(state => state.tasks[todoListId])
     const dispatch = useDispatch()
+
+    if (filter === "active") {
+        tasks = tasks.filter(f => !f.isDone)
+    }
+    if (filter === 'completed') {
+        tasks = tasks.filter(f => f.isDone)
+    }
 
     const addTasks = useCallback((title: string) => {
         dispatch(addTaskAC(todoListId, title))
     }, [dispatch, todoListId])
 
-    const removeTodoListHandler = () => {
+    const removeTodoListHandler = useCallback(() => {
         dispatch(removeTasksAC(todoListId))
         dispatch(removeTodoListAC(todoListId))
-    }
+    }, [dispatch, todoListId])
 
     const getNewTitleTodoList = useCallback((newTitle: string) => {
         dispatch(changedTodoListTitleAC(todoListId, newTitle))
     }, [dispatch, todoListId])
-
-    const changeStatusTasks = (todoListId: string, filter: FilterType): TasksType[] => {
-
-        if (filter === "active") {
-            return tasks[todoListId].filter(f => !f.isDone)
-        }
-        if (filter === 'completed') {
-            return tasks[todoListId].filter(f => f.isDone)
-        }
-        return tasks[todoListId]
-    }
 
     const taskFilterAll = useCallback(() => dispatch(tasksFilterAC(todoListId, 'all')), [dispatch, todoListId])
     const taskFilterActive = useCallback(() => dispatch(tasksFilterAC(todoListId, 'active')), [dispatch, todoListId])
@@ -61,31 +55,30 @@ export const Todolist: React.FC<TodolistPropsType> = memo((
         <div className={s.todoListWrapper}>
             <h3>
                 <SuperSpan title={titleTodo} callBack={getNewTitleTodoList}/>
-                <IconButton onClick={removeTodoListHandler} color={'primary'} size={'medium'}>
-                    <HighlightOffSharpIcon/>
-                </IconButton>
+                <IconMUIButton onClick={removeTodoListHandler} color={'primary'} size={'medium'}/>
             </h3>
             <AddItemForm getTitle={addTasks} label={'Add task'}/>
             <ul className={s.tasksWrapper}>
-                {changeStatusTasks(todoListId, filter).map((t, i) => <Task
-                    key={t.id}
-                    task={t}
-                    todoListId={todoListId}
-                    i={i}/>)}
+                {tasks.map((t, i) => {
+                    return <Task key={t.id}
+                                 task={t}
+                                 todoListId={todoListId}
+                                 i={i}/>
+                })}
             </ul>
             <div className={s.buttonWrapper}>
-                <SuperButton color={filter === 'all' ? 'secondary' : undefined}
-                             variant={'contained'}
-                             onClick={taskFilterAll}
-                             name={'All'}/>
-                <SuperButton color={filter === 'active' ? 'secondary' : undefined}
-                             variant={'contained'}
-                             onClick={taskFilterActive}
-                             name={'Active'}/>
-                <SuperButton color={filter === 'completed' ? 'secondary' : undefined}
-                             variant={'contained'}
-                             onClick={taskFilterCompleted}
-                             name={'Completed'}/>
+                <MUIButton color={filter === 'all' ? 'secondary' : undefined}
+                           variant={'contained'}
+                           onClick={taskFilterAll}
+                           name={'All'}/>
+                <MUIButton color={filter === 'active' ? 'secondary' : undefined}
+                           variant={'contained'}
+                           onClick={taskFilterActive}
+                           name={'Active'}/>
+                <MUIButton color={filter === 'completed' ? 'secondary' : undefined}
+                           variant={'contained'}
+                           onClick={taskFilterCompleted}
+                           name={'Completed'}/>
             </div>
         </div>
     );
