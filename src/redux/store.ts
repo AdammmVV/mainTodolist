6d98b67a3b1d2253a78d508914ef10c209eb0813
Adammm <adammm.web@gmail.com>
2023-04-15 01:47:54 +0300

    refactor added applyMiddleware and thuck for App

--- src/redux/store.ts
+++ src/redux/store.ts
@@ -1,14 +1,22 @@
-import {combineReducers, legacy_createStore} from "redux";
+import {AnyAction, applyMiddleware, combineReducers, legacy_createStore} from "redux";
 import {todoListReducer} from "./todoListReducer";
 import {tasksReducer} from "./tasksReducer";
+import thunk, {ThunkDispatch} from "redux-thunk";
+import {useDispatch} from "react-redux";
 
+// hooks
+export const useAppDispatch = () => useDispatch<ThunkDispatch<AppRootStateType, any, AnyAction>>()
+
+// Reducers
 const rootReducer = combineReducers({
     todoLists: todoListReducer,
     tasks: tasksReducer
 })
 
-export const store = legacy_createStore(rootReducer)
+// store
+export const store = legacy_createStore(rootReducer, applyMiddleware(thunk))
 
+// types
 export type AppRootStateType = ReturnType<typeof rootReducer>
 
 //@ts-ignore
