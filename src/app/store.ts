e95c3a726311bdd562e2e72537ca8f3545a9b6f2
Adammm <adammm.web@gmail.com>
2023-06-03 16:19:45 +0300

    update: store

--- src/app/store.ts
+++ src/app/store.ts
@@ -1,23 +1,31 @@
-import {AnyAction, applyMiddleware, combineReducers, legacy_createStore} from "redux";
-import {todoListReducer} from "features/Todolist/todoListReducer";
-import {tasksReducer} from "features/Todolist/Tasks/tasksReducer";
-import thunk, {ThunkDispatch} from "redux-thunk";
-import {useDispatch} from "react-redux";
-
-// hooks
-export const useAppDispatch = () => useDispatch<ThunkDispatch<AppRootStateType, any, AnyAction>>()
+import {
+  applyMiddleware,
+  combineReducers,
+  compose,
+  legacy_createStore,
+} from 'redux';
+import { todoListReducer } from 'features/todolist/todoListReducer';
+import { tasksReducer } from 'features/todolist/Tasks/tasksReducer';
+import thunk from 'redux-thunk';
+import { authReducer } from 'features/auth/authReducer';
+import { appReducer } from 'app/appReducer';
 
 // Reducers
 const rootReducer = combineReducers({
-    todoLists: todoListReducer,
-    tasks: tasksReducer
-})
+  app: appReducer,
+  todoLists: todoListReducer,
+  tasks: tasksReducer,
+  auth: authReducer,
+});
 
 // store
-export const store = legacy_createStore(rootReducer, applyMiddleware(thunk))
+export const store = legacy_createStore(
+  rootReducer,
+  compose(applyMiddleware(thunk))
+);
 
 // types
-export type AppRootStateType = ReturnType<typeof rootReducer>
+export type AppRootStateType = ReturnType<typeof rootReducer>;
 
 //@ts-ignore
-window.store = store
+window.store = store;
