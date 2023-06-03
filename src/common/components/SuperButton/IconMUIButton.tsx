28e1c162b9cfd528cf875a84b29f2e2fa7b7a589
Adammm <adammm.web@gmail.com>
2023-06-03 16:14:02 +0300

    update: IconMUIButton component

--- src/common/components/SuperButton/IconMUIButton.tsx
+++ src/common/components/SuperButton/IconMUIButton.tsx
@@ -1,22 +1,28 @@
-import React from "react";
-import {IconButton} from "@mui/material";
-import DeleteForeverSharpIcon from "@mui/icons-material/DeleteForeverSharp";
-
+import React from 'react';
+import { IconButton } from '@mui/material';
+import DeleteForeverSharpIcon from '@mui/icons-material/DeleteForeverSharp';
 
 type IconButtonPropsType = {
-    onClick: () => void
-    color: "primary" | "inherit" | "default" | "secondary" | "error" | "info" | "success" | "warning" | undefined
-    size?: "small" | "medium" | "large" | undefined
-}
+  onClick: () => void;
+  disabled: boolean
+  color:
+    | 'primary'
+    | 'inherit'
+    | 'default'
+    | 'secondary'
+    | 'error'
+    | 'info'
+    | 'success'
+    | 'warning'
+    | undefined;
+  size?: 'small' | 'medium' | 'large' | undefined;
+};
 export const IconMUIButton: React.FC<IconButtonPropsType> = React.memo(
-    ({
-         onClick,
-         color,
-         size,
-     }) => {
-        return (
-            <IconButton onClick={onClick} color={color} size={size}>
-                <DeleteForeverSharpIcon/>
-            </IconButton>
-        )
-    })
+  ({ onClick, color, size, disabled }) => {
+    return (
+      <IconButton onClick={onClick} color={color} size={size} disabled={disabled}>
+        <DeleteForeverSharpIcon />
+      </IconButton>
+    );
+  }
+);
