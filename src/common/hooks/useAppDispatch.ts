f3a5817266f562a8e8e974fd638a809a0f004652
Adammm <adammm.web@gmail.com>
2023-06-03 16:16:33 +0300

    create: useAppDispatch

---
+++ src/common/hooks/useAppDispatch.ts
@@ -0,0 +1,7 @@
+import { useDispatch } from 'react-redux';
+import { ThunkDispatch } from 'redux-thunk';
+import { AnyAction } from 'redux';
+import { AppRootStateType } from 'app/store';
+
+export const useAppDispatch = () =>
+  useDispatch<ThunkDispatch<AppRootStateType, any, AnyAction>>();
