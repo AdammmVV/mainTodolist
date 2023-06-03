34b6f733bdb609526ac54680176dd1dfb1260fa6
Adammm <adammm.web@gmail.com>
2023-06-03 16:13:48 +0300

    update: Header component

--- src/common/components/Header/Header.tsx
+++ src/common/components/Header/Header.tsx
@@ -1,30 +1,46 @@
-import React, {ChangeEvent, memo} from "react";
-import {AppBar, Button, IconButton, Switch, Toolbar, Typography} from "@mui/material";
-import MenuIcon from "@mui/icons-material/Menu";
+import React, { ChangeEvent, memo } from 'react';
+import {
+  AppBar,
+  Button,
+  IconButton,
+  Switch,
+  Toolbar,
+  Typography,
+} from '@mui/material';
+import MenuIcon from '@mui/icons-material/Menu';
+import { logoutTC } from 'features/auth/authReducer';
+import { useAppDispatch } from 'common/hooks/useAppDispatch';
+import { setDarkThemeAC } from 'app/appReducer';
 
-type HeaderPropsType = {
-    setDark: (checked: boolean) => void
-}
-export const Header: React.FC<HeaderPropsType> = memo(({setDark}) => {
-    const switchOnChangeHandler = (e: ChangeEvent<HTMLInputElement>) => setDark(e.currentTarget.checked)
+export const Header = memo(() => {
+  const dispatch = useAppDispatch();
 
-    return (
-        <AppBar position="static" color={'primary'}>
-            <Toolbar variant={'dense'}>
-                <IconButton
-                    size="large"
-                    edge="start"
-                    color="inherit"
-                    aria-label="menu"
-                    sx={{mr: 2}}>
-                    <MenuIcon/>
-                </IconButton>
-                <Typography variant="h6" component="div" sx={{flexGrow: 1}}>
-                    TodoList
-                </Typography>
-                <Switch onChange={switchOnChangeHandler} color={'secondary'}/>
-                <Button color="inherit">Login</Button>
-            </Toolbar>
-        </AppBar>
-    )
-})
+  const switchOnChangeHandler = (e: ChangeEvent<HTMLInputElement>) =>
+    dispatch( setDarkThemeAC(e.currentTarget.checked));
+
+  const onClickHandler = () => {
+    dispatch(logoutTC());
+  };
+  return (
+    <AppBar position="static" color={'primary'}>
+      <Toolbar variant={'dense'}>
+        <IconButton
+          size="large"
+          edge="start"
+          color="inherit"
+          aria-label="menu"
+          sx={{ mr: 2 }}
+        >
+          <MenuIcon />
+        </IconButton>
+        <Typography variant="h6" component="div" sx={{ flexGrow: 1 }}>
+          TodoList
+        </Typography>
+        <Switch onChange={switchOnChangeHandler} color={'secondary'} />
+        <Button color="inherit" onClick={onClickHandler}>
+          Login
+        </Button>
+      </Toolbar>
+    </AppBar>
+  );
+});
