72bfcf4dc4900e012b455f59897ade461d6e56ca
Adammm <adammm.web@gmail.com>
2023-06-03 16:18:58 +0300

    create: Layout component

---
+++ src/app/components/Layout.tsx
@@ -0,0 +1,38 @@
+import React, { useEffect } from 'react';
+import { useAppDispatch } from 'common/hooks/useAppDispatch';
+import { useAppSelector } from 'common/hooks/useAppSelector';
+import { getMeTC } from "features/auth/authReducer";
+import { getTodoListAT } from 'features/todolist/todoListReducer';
+import Container from '@mui/material/Container/Container';
+import { Outlet, useNavigate } from "react-router-dom";
+import { paths } from "common/constants/paths";
+import { Loader } from 'common/components/Loader/Loader';
+import { Header } from 'common/components/Header/Header';
+
+
+export const Layout = () => {
+  const dispatch = useAppDispatch();
+  const isAuth = useAppSelector((state) => state.auth.isAuth);
+  const isLoading = useAppSelector(state => state.app.isLoading)
+  const navigate = useNavigate()
+
+  useEffect(() => {
+    dispatch(getMeTC())
+    if (!isAuth) {
+      navigate(paths.LOGIN)
+      return
+    }
+    dispatch(getTodoListAT());
+    navigate(paths.MAIN)
+  }, [isAuth, dispatch, navigate]);
+
+
+  return (
+    <>
+      <Header />
+      <Container fixed sx={{ p: 10 }}>
+        {isLoading ? <Loader /> : <Outlet />}
+      </Container>
+    </>
+  );
+};
