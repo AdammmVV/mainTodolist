8c3cb255bc013607bee1a2f13049eff804326eba
Adammm <adammm.web@gmail.com>
2023-06-10 17:53:20 +0300

    refactor: Header

--- src/common/components/Header/Header.tsx
+++ src/common/components/Header/Header.tsx
@@ -1,25 +1,18 @@
 import React, { ChangeEvent, memo } from 'react';
-import {
-  AppBar,
-  Button,
-  IconButton,
-  Switch,
-  Toolbar,
-  Typography,
-} from '@mui/material';
+import { AppBar, Button, IconButton, Switch, Toolbar, Typography } from '@mui/material';
 import MenuIcon from '@mui/icons-material/Menu';
-import { logoutTC } from 'features/auth/authReducer';
 import { useAppDispatch } from 'common/hooks/useAppDispatch';
-import { setDarkThemeAC } from 'app/appReducer';
+import { authThunk } from 'features/auth/auth.slice';
+import { appActions } from 'app/app.slice';
 
 export const Header = memo(() => {
   const dispatch = useAppDispatch();
 
-  const switchOnChangeHandler = (e: ChangeEvent<HTMLInputElement>) =>
-    dispatch( setDarkThemeAC(e.currentTarget.checked));
+  const onChangeTheme = (e: ChangeEvent<HTMLInputElement>) =>
+    dispatch( appActions.setTheme({ theme: e.currentTarget.checked }));
 
   const onClickHandler = () => {
-    dispatch(logoutTC());
+    dispatch(authThunk.logout());
   };
   return (
     <AppBar position="static" color={'primary'}>
@@ -36,7 +29,7 @@ export const Header = memo(() => {
         <Typography variant="h6" component="div" sx={{ flexGrow: 1 }}>
           TodoList
         </Typography>
-        <Switch onChange={switchOnChangeHandler} color={'secondary'} />
+        <Switch onChange={onChangeTheme} color={'secondary'} />
         <Button color="inherit" onClick={onClickHandler}>
           Login
         </Button>
