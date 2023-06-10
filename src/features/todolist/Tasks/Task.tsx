c5d04c68cba394a7faf3f2253e75c7ea060b2b27
Adammm <adammm.web@gmail.com>
2023-06-10 17:56:18 +0300

    refactor: Task

--- src/features/todolist/Tasks/Task.tsx
+++ src/features/todolist/Tasks/Task.tsx
@@ -1,7 +1,3 @@
-import {
-  removeTaskAT,
-  updateTaskAT,
-} from 'features/todolist/Tasks/tasksReducer';
 import React, { ChangeEvent, memo, useCallback } from 'react';
 import s from 'features/todolist/Todolist.module.css';
 import { Checkbox } from '@mui/material';
@@ -9,42 +5,43 @@ import { SuperSpan } from 'common/components/SuperSpan/SuperSpan';
 import { IconMUIButton } from 'common/components/SuperButton/IconMUIButton';
 import { TaskDomainType, TaskStatuses } from 'api/api';
 import { useAppDispatch } from 'common/hooks/useAppDispatch';
+import { taskThunk } from 'features/todolist/Tasks/tasks.slice';
 
 type TaskPropsType = {
   task: TaskDomainType;
   todoListId: string;
 };
-export const Task = memo((props: TaskPropsType) => {
+export const Task: React.FC<TaskPropsType> = memo(({todoListId, task}) => {
   const dispatch = useAppDispatch();
 
   const changeStatusCheckbox = (e: ChangeEvent<HTMLInputElement>) => {
     const status = e.currentTarget.checked
       ? TaskStatuses.New
       : TaskStatuses.Completed;
-    dispatch(updateTaskAT(props.todoListId, props.task.id, { status }));
+    dispatch(taskThunk.updateTask({todoListId, taskId: task.id, domainModel: {status}}));
   };
 
   const removeTaskHandler = useCallback(
-    () => dispatch(removeTaskAT(props.todoListId, props.task.id)),
-    [dispatch, props.todoListId, props.task.id]
+    () => dispatch(taskThunk.removeTask({ todoListId, taskId: task.id })),
+    [dispatch, todoListId, task.id]
   );
 
   const changeTitle = useCallback(
     (title: string) =>
-      dispatch(updateTaskAT(props.todoListId, props.task.id, { title })),
-    [dispatch, props.todoListId, props.task.id]
+      dispatch(taskThunk.updateTask({ todoListId, taskId: task.id, domainModel: {title} })),
+    [dispatch, todoListId, task.id]
   );
 
-  const finalStyle = `${props.task.status ? `${s.taskWrapper} ${s.done}` : s.taskWrapper}`
+  const finalStyle = `${task.status ? `${s.taskWrapper} ${s.done}` : s.taskWrapper}`
 
   return (
     <li className={finalStyle}>
       <div>
         <Checkbox
           onChange={changeStatusCheckbox}
-          checked={props.task.status === 0}
+          checked={task.status === TaskStatuses.New}
         />
-        <SuperSpan title={props.task.title} callBack={changeTitle} />
+        <SuperSpan title={task.title} callBack={changeTitle} />
       </div>
       <IconMUIButton onClick={removeTaskHandler} color={'primary'}/>
     </li>
