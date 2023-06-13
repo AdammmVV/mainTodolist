7cb56932c57c66f99ae16fc146db9750b85218ff
Adammm <adammm.web@gmail.com>
2023-06-13 22:45:29 +0300

    refactor: Header

--- src/common/components/Header/Header.tsx
+++ src/common/components/Header/Header.tsx
@@ -1,38 +1,23 @@
-import React, { ChangeEvent, memo } from 'react';
-import { AppBar, Button, IconButton, Switch, Toolbar, Typography } from '@mui/material';
-import MenuIcon from '@mui/icons-material/Menu';
-import { useAppDispatch } from 'common/hooks/useAppDispatch';
-import { authThunk } from 'features/auth/auth.slice';
-import { appActions } from 'app/app.slice';
+import React, { memo } from 'react';
+import { AppBar, Button, Toolbar, Typography } from '@mui/material';
+import { ThemeSwitch } from '../ThemeSwitch/ThemeSwitch';
+import { useAppTheme } from 'app/hooks/useAppTheme';
+import { useAuth } from 'features/auth/hooks/useAuth';
 
 export const Header = memo(() => {
-  const dispatch = useAppDispatch();
+  const { theme, onChangeTheme } = useAppTheme();
+  const { isAuth, onLogout } = useAuth()
 
-  const onChangeTheme = (e: ChangeEvent<HTMLInputElement>) =>
-    dispatch( appActions.setTheme({ theme: e.currentTarget.checked }));
-
-  const onClickHandler = () => {
-    dispatch(authThunk.logout());
-  };
   return (
-    <AppBar position="static" color={'primary'}>
+    <AppBar position='static' color={'primary'}>
       <Toolbar variant={'dense'}>
-        <IconButton
-          size="large"
-          edge="start"
-          color="inherit"
-          aria-label="menu"
-          sx={{ mr: 2 }}
-        >
-          <MenuIcon />
-        </IconButton>
-        <Typography variant="h6" component="div" sx={{ flexGrow: 1 }}>
+        <Typography variant='h6' component='div' sx={{ flexGrow: 1 }}>
           TodoList
         </Typography>
-        <Switch onChange={onChangeTheme} color={'secondary'} />
-        <Button color="inherit" onClick={onClickHandler}>
-          Login
-        </Button>
+        <ThemeSwitch onChange={onChangeTheme} checked={theme} />
+        {isAuth && <Button color='inherit' onClick={onLogout}>
+          Logout
+        </Button>}
       </Toolbar>
     </AppBar>
   );
