4d495689261ac828dd07d7eb29474bf28c15156b
Adammm <adammm.web@gmail.com>
2023-06-16 01:16:38 +0300

    change: for gh-pages (hashRouter)

--- src/common/routes/Routes.tsx
+++ src/common/routes/Routes.tsx
@@ -1,12 +1,12 @@
 import React from 'react';
-import { createBrowserRouter } from 'react-router-dom';
+import { createHashRouter } from 'react-router-dom';
 import { TodolistList } from 'features/todolist/components/TodolistList';
 import { Login } from 'features/auth/components/Login/Login';
 import { Layout } from 'app/components/Layout';
 import { paths } from 'common/constants/paths';
 import { NotFound } from 'common/components/NotFound/NotFound';
 
-export const router = createBrowserRouter([
+export const router = createHashRouter([
   {
     path: paths.MAIN,
     element: <Layout />,
