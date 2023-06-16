24847902f2fa0abb6d2bff5759953faefb01171a
Adammm <adammm.web@gmail.com>
2023-06-16 13:08:17 +0300

    refactor: Layout

--- src/app/components/Layout.tsx
+++ src/app/components/Layout.tsx
@@ -2,31 +2,22 @@ import React, { useEffect } from 'react';
 import { useAppDispatch } from 'common/hooks/useAppDispatch';
 import { useAppSelector } from 'common/hooks/useAppSelector';
 import Container from '@mui/material/Container/Container';
-import { Outlet, useNavigate } from 'react-router-dom';
-import { paths } from 'common/constants/paths';
+import { Outlet } from 'react-router-dom';
 import { LoaderApp } from 'common/components/Loaders/loaderApp/LoaderApp';
 import { Header } from 'common/components/Header/Header';
 import LinearProgress from '@mui/material/LinearProgress/LinearProgress';
-import { todoListThunk } from 'features/todolist/todoList.slice';
 import { isAppLoadingSelector, isLoadingSelector } from 'app/app.selectors';
-import { useAuth } from 'features/auth/hooks/useAuth';
 import { AppNotify } from 'common/components/AppNotify/AppNotify';
+import { authThunks } from 'features/auth/auth.slice';
 
 export const Layout = () => {
-  const { isAuth, onAuth } = useAuth();
-  const navigate = useNavigate();
   const dispatch = useAppDispatch();
   const isLoading = useAppSelector(isLoadingSelector);
   const isAppLoading = useAppSelector(isAppLoadingSelector);
 
   useEffect(() => {
-    if (!isAuth) {
-      onAuth();
-      return;
-    }
-    navigate(paths.MAIN);
-    dispatch(todoListThunk.getTodoList());
-  }, [isAuth, dispatch, navigate]);
+      dispatch(authThunks.getMe())
+  }, [dispatch]);
 
   return (
     <>
