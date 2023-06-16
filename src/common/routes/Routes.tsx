0b9a4018060dac6dc0fe20868d27249d94404efc
Adammm <adammm.web@gmail.com>
2023-06-16 13:08:39 +0300

    refactor: Routers

--- src/common/routes/Routes.tsx
+++ src/common/routes/Routes.tsx
@@ -5,21 +5,33 @@ import { Login } from 'features/auth/components/Login/Login';
 import { Layout } from 'app/components/Layout';
 import { paths } from 'common/constants/paths';
 import { NotFound } from 'common/components/NotFound/NotFound';
+import { RequireAuth } from 'common/routes/RequireAuth';
+import { Auth } from './Auth';
 
 export const router = createHashRouter([
   {
     path: paths.MAIN,
     element: <Layout />,
-    errorElement: <NotFound/>,
+    errorElement: <NotFound />,
     children: [
       {
-        path: paths.MAIN,
-        element: <TodolistList />,
+        element: <RequireAuth />,
+        children: [
+          {
+            path: paths.MAIN,
+            element: <TodolistList />
+          }
+        ]
       },
       {
-        path: paths.LOGIN,
-        element: <Login />,
-      },
-    ],
-  },
+        element: <Auth />,
+        children: [
+          {
+            path: paths.LOGIN,
+            element: <Login />
+          }
+        ]
+      }
+    ]
+  }
 ]);
